f86acd9d0ddf9b59f74be061086812bbd30495f1
Aswini Sankar <53120389+AswiniSankar@users.noreply.github.com>
2022-02-27 17:53:04 +1200

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,11 @@ from plyer import notification
 import time
 
 def main():
-    # print('Hello world')
-    # title = 'Hello Amazing people!'
-    # message= 'Thankyou for reading! Take care!'
     while True:
         notification.notify(
             title = "Hello Amazing people!",
             message = "Take a break! It has been an hour!",
-             app_icon = "/home/aswini/Downloads/heart-icon_34407.ico",
+            app_icon = "heart-icon_34407.ico",
             timeout = 10
         )
         time.sleep(10)
